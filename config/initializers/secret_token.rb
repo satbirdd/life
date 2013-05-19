9ac00371a20a89e7577c9335b7a76cda8dcc9859
liulei <liulei@liulei-NF520-A2G.(none)>
2013-05-19 19:44:04 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Life::Application.config.secret_token = '7c7592a6d16ad39834821c6763fd76858f5882f907188e432132891a135fb28fc1904718cdaec5199ab1bf155fc3798fef9f784e5f7fc8408a6d6eab288d0e2e'
